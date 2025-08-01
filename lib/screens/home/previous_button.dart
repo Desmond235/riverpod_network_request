773adc9232f812bf11411f601594634205e5978f
Desmond <adabedesmond@gmail.com>
2025-07-31 18:31:11 -0800

    Add PreviousButton component for navigating to the previous page

---
+++ lib/screens/home/previous_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_network_request/provider/page_index_provider.dart';
+
+class PreviousButton extends ConsumerWidget {
+  const PreviousButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final canGoToPreviousPage = ref.watch(canGoToPreviousPageProvider);
+
+    void goToPreviousPage() {
+      final index =ref.read(pageIndexProvider.notifier).update((state) => state - 1);
+    }
+
+    return ElevatedButton(
+      onPressed: canGoToPreviousPage ? goToPreviousPage : null,
+      child: const Text('Previous'),
+    );
+  }
+}
